d364714069f020faaa1f9c9d76a6a031159aac06
Streetwalrus Einstein <streetwalrus@codewalr.us>
2015-10-22 00:42:20 +0300

    Print version on startup

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "render.h"
 #include "misc.h"
+#include "version.h"
 
 int main(int argc, char **argv)
 {
     UNUSED(argc);
     UNUSED(argv);
 
+    printf("%s r%s (%s-%s)\n", PROJ_NAME, git_rev, git_branch, git_commit);
     render_loop();
 
     return EXIT_SUCCESS;
